c71e5346649cec441ee56c00c77dd32b922a9b43
amanig3 <111730602+amanig3@users.noreply.github.com>
2023-01-25 12:27:34 +0100

    Arbiedskrav

---
+++ ressurser.js
@@ -0,0 +1,128 @@
+const resources = [
+    {
+        category: "HTML",
+        text: "HTML står for HyperText Markup Language, og er et strukturspråk som brukes for å lage strukturer til nettside- og applikasjonsgrensesnitt.",
+        sources: [
+            {
+                title: "W3Schools",
+                url: "https://www.w3schools.com/html/"
+            },
+            {
+                title: "HTML Living standard",
+                url: "https://html.spec.whatwg.org/multipage/"
+            },
+            {
+                title: "HTML.com Tutorials",
+                url: "https://html.com/"
+            },
+        ]
+    },
+    {
+        category: "CSS",
+        text: "CSS står for Cascading StyleSheets, og brukes for å sette stilregler på HTML-elementer.",
+        sources: [
+            {
+                title: "W3Schools",
+                url: "https://www.w3schools.com/css/"
+            },
+            {
+                title: "W3C HTML & CSS Standards",
+                url: "https://www.w3.org/standards/webdesign/htmlcss.html"
+            },
+            {
+                title: "W3C CSS Validator",
+                url: "https://jigsaw.w3.org/css-validator/"
+            },
+            {
+                title: "CSS Tricks",
+                url: "https://css-tricks.com/"
+            },
+        ]
+    },
+    {
+        category: "JavaScript",
+        text: "JavaScript er et scriptspråk basert på EcmaScript. JavaScript kjører direkte i nettleseren, og brukes ofte til å manipulere HTML og CSS i webgrensnesnitt.",
+        sources: [
+            {
+                title: "W3Schools",
+                url: "https://www.w3schools.com/js/"
+            },
+            {
+                title: "MDN Web Docs",
+                url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+            },
+            {
+                title: "How to read JavaScript Documentation",
+                url: "https://www.youtube.com/watch?v=O3iR-CIufKM"
+            },
+        ]
+    },
+    {
+        category: "React",
+        text: "React er et rammeverk bygget i JavaScript. React bruker komponenter og states for å lage en levende frontend.",
+        sources: [
+            {
+                title: "React documentation",
+                url: "https://reactjs.org/docs/getting-started.html"
+            },
+            {
+                title: "W3Schools",
+                url: "https://www.w3schools.com/REACT/DEFAULT.ASP"
+            },
+            {
+                title: "How to read JavaScript Documentation",
+                url: "https://www.youtube.com/watch?v=O3iR-CIufKM"
+            },
+        ]
+    },
+    {
+        category: "Sanity and headless CMS",
+        text: "Sanity er et headless CMS som står for innholdsadministrasjon. Innhold hentes inn i applikasjoner via GROQ-spørringer.",
+        sources: [
+            {
+                title: "Sanity documentation",
+                url: "https://www.sanity.io/docs"
+            },
+            {
+                title: "OnCrawl: a beginners guide to headless CMS",
+                url: "https://www.oncrawl.com/technical-seo/beginners-guide-headless-cms/"
+            },
+            {
+                title: "Section.io: Getting started with Sanity CMS",
+                url: "https://www.section.io/engineering-education/getting-started-with-sanity-cms/"
+            },
+        ]
+    },
+]
+
+const navimenu = document.getElementById("navimenu")
+// Dette her er en variabel som er laget for å kunne hente informajson fra arrayen
+let linkmenuHTML = ``;
+resources.map((category) => {
+
+    let = infoMenu = ``;
+    category.sources.forEach((pro) => {
+        infoMenu += `
+        <li><a target="blank" href=${pro.url} >${pro.title}</a></li>`;
+        // Med denne koden her ^ henter jeg både linken og tittelen på linken
+    });
+
+    linkmenuHTML += `
+    <section class="urlLinks">
+    <button>${category.category}</button>
+    <div class="open">
+    <h1>${category.category}</h1>
+    <p class="text">${category.text}</p>
+    <ul class="infoMenu">
+    ${infoMenu}
+    </ul>
+    </div>
+ </section>`;
+ // I denne koden ^ beskriver jeg hvordan ting skal ligge på nettside, og det blir hentet fra arrayen over
+})
+
+navimenu.innerHTML = `<ul class="boxMenu">${linkmenuHTML}</ul>`;
+// Denne koden her ^ leser hele javascript koden og sender det videre til nettsiden slik at det vises der
+
+// I denne javascript koden har jeg fått hjelp av hjelpelærer siden jeg har vært på øvingstimene
+
